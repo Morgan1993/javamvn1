3ffc36bcacc817ab06bfb9f68c9dd935edd32a2c
Евгений Моргунов <morgunov-zhenya@mail.ru>
2022-06-21 21:26:20 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService bonusService = new BonusService();
+        long bonus = bonusService.calculate(1000,true);
+        System.out.println(bonus);
+    }
+}
